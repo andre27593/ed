ae512b7cdaea585cce3ff72d849d3b8fbf23e81f
andre27593 <dam.aolivero@institutoserpis.org>
2016-05-11 09:42:14 +0200

    CCategoria

--- CCategoria/CCategoria/MainWindow.cs
+++ CCategoria/CCategoria/MainWindow.cs
@@ -14,55 +14,32 @@ public partial class MainWindow: Gtk.Window
 		treeView.AppendColumn ("id", new CellRendererText (), "text", 0);
 		treeView.AppendColumn ("nombre", new CellRendererText(), "text", 1);
 
-		ListStore liststore = new ListStore(typeof(long), typeof(string));
-		treeView.Model = liststore;
+		ListStore listStore = new ListStore(typeof(long), typeof(string));
+		treeView.Model = listStore;
 
 		//liststore.AppendValues (33L, "Treinta y tres");
 
 		App.Instance.DbConnection = new MySqlConnection ("Database=dbprueba;user=root;password=sistemas");
 		App.Instance.DbConnection.Open ();
 
-		//operaciones (insert, select...) 
+		fillListStore (listStore);
 
-		//IDbCommand insertDbCommand = dbConnection.CreateCommand ();
-		//insertDbCommand.CommandText = "insert into categoria (nombre) values ('categoria 2')";
-		//insertDbCommand.ExecuteNonQuery ();
 
-		//Las operaciones que toquen
-
-		IDbCommand dbcommand = App.Instance.DbConnection.CreateCommand ();
-		dbcommand.CommandText = "select * from categoria order by id";
-		IDataReader dataReader = dbcommand.ExecuteReader ();
-		while (dataReader.Read()) {
-			//Console.WriteLine ("id={0} nombre={1}", dataReader ["id"], dataReader ["nombre"]);
-			liststore.AppendValues (dataReader ["id"], dataReader ["nombre"]);
-		}
-		dataReader.Close ();
 		newAction.Activated += delegate {
 			CCategoria.CategoriaView categoriaView = new CCategoria.CategoriaView(); 
 		};
 
 		deleteAction.Activated += delegate {
 
-			//confirmar
-//
-//			MessageDialog messageDialog = new MessageDialog(this, DialogFlags.Modal, MessageType.Question, ButtonsType.YesNo, "Quieres eliminar el registro?");
-//
-//			messageDialog.Title = Title;
-//			ResponseType response = (ResponseType)messageDialog.Run();
-//			messageDialog.Destroy();
-//
-//			if(response != ResponseType.Yes){
-//				return;
-//			}
 
-			if (!confirm(this, "Quieres eliminar el registro?")){
+
+			if (!WindowHelper.Confirm(this, "Quieres eliminar el registro?")){
 				return;
 			}
 
 			TreeIter treeIter;
 		    treeView.Selection.GetSelected(out treeIter);
-			object id = liststore.GetValue(treeIter,0);
+			object id = listStore.GetValue(treeIter,0);
 
 			Console.WriteLine ("deleteAction Activated id = {0}" , id);
 			IDbCommand deleteDbCommand = App.Instance.DbConnection.CreateCommand();
@@ -72,6 +49,10 @@ public partial class MainWindow: Gtk.Window
 			deleteDbCommand.ExecuteNonQuery();
 
 		};
+
+		refreshActions.Activated += delegate {
+			fillListStore (listStore);
+	};
 
 		treeView.Selection.Changed += delegate {
 			Console.WriteLine ("treeView  Selection Changed CountSelectedRows()={0}",
@@ -84,16 +65,7 @@ public partial class MainWindow: Gtk.Window
 
 	}
 
-	private bool confirm(Window parent, string message){
-		MessageDialog messageDialog = new MessageDialog(parent, DialogFlags.Modal, MessageType.Question, ButtonsType.YesNo, message);
 
-		messageDialog.Title = parent.Title;
-		ResponseType response = (ResponseType)messageDialog.Run();
-		messageDialog.Destroy();
-
-
-		return response == ResponseType.Yes;
-	}
 
 	private void refreshAction(){
 
@@ -101,8 +73,18 @@ public partial class MainWindow: Gtk.Window
 
 	}
 
-	private void delete(){
+	private void fillListStore(ListStore listStore){
+		listStore.Clear ();
 
+		IDbCommand dbcommand = App.Instance.DbConnection.CreateCommand ();
+		dbcommand.CommandText = "select * from categoria order by id";
+		IDataReader dataReader = dbcommand.ExecuteReader ();
+		while (dataReader.Read()) {
+			//Console.WriteLine ("id={0} nombre={1}", dataReader ["id"], dataReader ["nombre"]);
+			listStore.AppendValues (dataReader ["id"], dataReader ["nombre"]);
+		}
+		dataReader.Close ();
+	
 	}
 
 
--- CCategoria/CCategoria/WindowHelper.cs
+++ CCategoria/CCategoria/WindowHelper.cs
@@ -1,11 +1,20 @@
+using Gtk;
 using System;
+
 
 namespace CCategoria
 {
 	public class WindowHelper
 	{
-		public WindowHelper ()
-		{
+		public static bool Confirm(Window parent, string message){
+			MessageDialog messageDialog = new MessageDialog(parent, DialogFlags.Modal, MessageType.Question, ButtonsType.YesNo, message);
+
+			messageDialog.Title = parent.Title;
+			ResponseType response = (ResponseType)messageDialog.Run();
+			messageDialog.Destroy();
+
+
+			return response == ResponseType.Yes;
 		}
 	}
 }
